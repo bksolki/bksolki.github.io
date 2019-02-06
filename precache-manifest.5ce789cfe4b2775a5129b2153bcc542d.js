7092f5cb5b12dff305167b0d8d9e0481aa2846cc
Emma <bksolki@gmail.com>
2019-02-06 22:38:37 +0700

    live for online test

---
+++ precache-manifest.5ce789cfe4b2775a5129b2153bcc542d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b7e0b120e4ad5a3fe93",
+    "url": "/static/js/main.3b7e0b12.chunk.js"
+  },
+  {
+    "revision": "52866185c59f770488ad",
+    "url": "/static/js/1.52866185.chunk.js"
+  },
+  {
+    "revision": "3b7e0b120e4ad5a3fe93",
+    "url": "/static/css/main.88aa6c23.chunk.css"
+  },
+  {
+    "revision": "52866185c59f770488ad",
+    "url": "/static/css/1.dd8042cf.chunk.css"
+  },
+  {
+    "revision": "f7f36f9900a740c60bf449b09960aa05",
+    "url": "/index.html"
+  }
+];
